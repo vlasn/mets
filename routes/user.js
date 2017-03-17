4c71b2854e14a5b3a19dafa47aeb3a539ffc4e5c
vlasn <vlasn@tlu.ee>
2017-03-17 21:48:44 +0200

    This is before installing react - one route.

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,19 @@
 const express = require('express');
 const router = express.Router();
 
+//    localhost:3080/api/user
 
 router.get("/", function(req,res) {
-    return res.json("users endpoint operational")
+    /*
+        Most of the "real" backend code will sit in files like this - this is to keep the server.js small and
+        lean, and to keep things modular. Breaking this file would not break other endpoints.
+     */
+    return res.json("users endpoint operational - brought to you by Express Router!")
 });
 
+
 module.exports = router;
+
+
+
+
--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const express = require('express');
 const app = express();
 
 
-//Attempting to implement a route
+//Implementing a route - first saving the correct route module to a const:
 const user = require('./routes/user');
+//and then pulling it in - in case of /api/user, we're going to return the module stored above.
 app.use('/api/user', user);
 
-
-
+app.get('/api', function(req,res){
+   res.json('Looks like the routing works - hi, this is Node. Now try the /user endpoint!')
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
