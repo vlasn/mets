60e21c7ad1b2e15f3ec3edc36c4f8aef340e76e9
thetloffline <root@Thetloffline.local>
2017-03-20 18:30:58 +0200

    edited the example

--- src/app.js
+++ src/app.js
@@ -4,17 +4,25 @@ const css = require('./app.scss');
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Component } from "./components/component";
+import { MidaIganes } from "./components/midaIganes";
+import { VeelMidagi } from "./components/veelMidagi";
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <h1>Hi</h1>
-                <Component number = {5}/>
+                <MidaIganes
+                	number = {66}
+                />
+                <VeelMidagi 
+	                string = 'see on komponendi VeelMidagi Paragraph-i props.string ja kohe siin otsas on props.number: ' 
+	                number = {666}
+                />
             </div>
         );
     }
 }
+
 
 ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ src/components/midaIganes.js
@@ -0,0 +1,24 @@
+/**
+ * Created by clstrfck, edited by thetloffline on 20/03/2017.
+ */
+import React from "react"
+const css = require('./midaIganes.scss');
+
+export class MidaIganes extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className="midaIganes">
+                See on Div. Ja see on MidaIganes objekti külge lisatan props.number: {this.props.number}
+                <p className="midaIganes__p">Ja see on Paragraph, millel on CSS hover: yellow.</p>
+                <button className="midaIganes__btn">Button</button>
+            </div>
+        )
+    }
+}
+
+MidaIganes.propTypes = {
+    number: React.PropTypes.number //.isRequired kui prop on kohustuslik
+};
