008d68fb11f81c9b4ad354e49169e1398539bb6e
vlasn <vlasn@tlu.ee>
2017-03-29 20:35:54 +0300

    Reinitializing

--- src/app.js
+++ src/app.js
@@ -4,21 +4,11 @@ const css = require('./app.scss');
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { MidaIganes } from "./components/midaiganes";
-import { VeelMidagi } from "./components/veelmidagi";
-
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hi</h1>
-                <MidaIganes
-                	number = {66}
-                />
-                <VeelMidagi 
-	                string = 'see on komponendi VeelMidagi Paragraph-i props.string ja kohe siin otsas on props.number: ' 
-	                number = {666}
-                />
+
             </div>
         );
     }
