836d27ecad2965a80b1bcb30b6811d35ebe80901
hensav <hensav@gmail.com>
2017-04-19 12:06:18 +0300

    Landing page

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ import { BrowserRouter as Router, Link, Route } from "react-router-dom"
 import Login from "./components/Login"
 import store from "./store"
 import Validate from "./components/Validate"
+import Landing from "./components/Landing"
+
 
 class App extends React.Component {
     render() {
@@ -16,10 +18,14 @@ class App extends React.Component {
                 <div className="main__wrapper">
                     <Link to={'/login'}>Login</Link>
                     <Link to={'/newuser'}>Kasutaja valideerimine</Link>
+                    <Link to={'/landing'}>Landing page</Link>
 
                     <Route path="/login" component={Login}/>
                     <Route path="/newuser" component={Validate}/>
+                    <Route path="/landing" component={Landing}/>
                     <Route path="/" exact={true} render={()=>(<p>Lingid kiireks vaadete vahetamiseks on praegu siin kohal.</p>)}/>
+                    <Landing/>
+
                 </div>
             </Router>
         );
