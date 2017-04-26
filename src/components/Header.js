babde43c7976b0b1e06e8063cb4018ae1bfc0c5a
thetloffline <root@thetloffline.lan>
2017-04-26 14:25:58 +0300

    changed header.js button padding and style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import MenuItem from 'material-ui/MenuItem';
-import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import Popover from 'material-ui/Popover';
 import Menu from 'material-ui/Menu';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 let injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 
+const labelStyles = {
+    headerButton: {
+        color: 'white'
+    }
+}
+
 const styles = {
     headerDropdown: {
         float: 'right',
         margin:'10px',
+        padding: '0px 10px 0px 10px',
+        height: '45px',
         display:'inline-block',
+        boarderRadius:'0px',
     },
+
     headerButton: {
         float: 'left',
         margin:'10px',
+        padding: '0px 10px 0px 10px',
+        height: '45px',
         display:'inline-block',
         boarderRadius:'0px',
     },
     container: {
-        padding:'30px',
+        padding: '1px 0px 50px 0px',
         textAlign:'center',
         width:'100vw',
         //position: 'fixed',
         //zIndex: '1',
     },
+
 };
+
 export default class Header extends React.Component {
 
     constructor(props) {
@@ -61,23 +75,26 @@ export default class Header extends React.Component {
         });
     };
 
-
     render() {
         return (
             <MuiThemeProvider>
                 <div className = "Header__wrapper" style={styles.container}>
-                    <RaisedButton 
+                    <FlatButton 
                         label="Avaleht"
                         style={styles.headerButton}
-                        backgroundColor= "#64DD17"
-                        labelColor = "white"
+                        backgroundColor= "black"
+                        hoverColor = "#00CC33"
+                        labelStyle ={labelStyles.headerButton}
+                        
                     />
-                    <RaisedButton
+                    <FlatButton
                         onTouchTap={this.handleTouchTap}
                         label="Henry"
                         style={styles.headerDropdown}
-                        backgroundColor= "#64DD17"
-                        labelColor = "white"
+                        backgroundColor= "#00CC33"
+                        hoverColor = "#009933"
+                        labelStyle ={labelStyles.headerButton}
+                        
                     />
                     <Popover
                         open={this.state.open}
