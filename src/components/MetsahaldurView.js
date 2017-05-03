72db94c7a00942dc07ee3cfd4592ae726d7f26a9
hensav <hensav@gmail.com>
2017-05-03 11:58:27 +0300

    MetsahaldurView with stylesheet

--- src/components/MetsahaldurView.js
+++ src/components/MetsahaldurView.js
@@ -3,35 +3,30 @@ import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import MetsahaldurViewTable from "./MetsahaldurViewTable"
 import FlatButton from 'material-ui/FlatButton';
 import Divider from 'material-ui/Divider';
-import MetsahaldurViewTab from "./MetsahaldurViewTab"
+
+const css = require("./MetsahaldurView.scss")
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const styles = {
-    container: {
-        textAlign:'center',
-    },
-
-}
 
 
 const MetsahaldurView = () => (
-    <MuiThemeProvider>
+    <div className = "MetsahaldurView__wrapper">
+        <MuiThemeProvider>
 
-        <Card style={styles.container}>
-            <CardHeader
-                title="Mets 1"
-                //subtitle="Subtitle"
-                actAsExpander={true}
-                showExpandableButton={true}
-            />
-
-            <CardText expandable={true}>
-                {/*<MetsahaldurViewTable/>*/}
-            </CardText>
-        </Card>
-
-    </MuiThemeProvider>
+            <Card>
+                <CardHeader
+                    title="Mets 1"
+                    //subtitle="Subtitle"
+                    actAsExpander={true}
+                    showExpandableButton={true}
+                />
+                <CardText expandable={true}>
+                    {/*<MetsahaldurViewTable/>*/}
+                </CardText>
+            </Card>
+        </MuiThemeProvider>
+    </div>
 );
 
 export default MetsahaldurView;
