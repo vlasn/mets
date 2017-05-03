eae4fdddc73673f424c6215f8bb9a7c35c124845
vlasn <vlasn@tlu.ee>
2017-05-03 11:59:34 +0300

    Started building custom nav wrapper thing

--- src/components/MetsahaldurView.js
+++ src/components/MetsahaldurView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton'
 import MetsahaldurViewTable from "./MetsahaldurViewTable"
-import FlatButton from 'material-ui/FlatButton';
 import Divider from 'material-ui/Divider';
 import MetsahaldurViewTab from "./MetsahaldurViewTab"
 
@@ -18,16 +18,31 @@ const styles = {
 const MetsahaldurView = () => (
     <MuiThemeProvider>
 
-        <Card style={styles.container}>
+        <Card >
             <CardHeader
                 title="Mets 1"
                 //subtitle="Subtitle"
                 actAsExpander={true}
                 showExpandableButton={true}
+                style={styles.container}
             />
 
             <CardText expandable={true}>
                 {/*<MetsahaldurViewTable/>*/}
+                <FlatButton
+                    label="Leping"
+                    hoverColor = "#009933"
+                />
+                <FlatButton
+                    label="Hinnatabel"
+                />
+                <FlatButton
+                    label="Metsateatis"
+                />
+                <FlatButton
+                    label="Vaata koondakti"
+                />
+
             </CardText>
         </Card>
 
