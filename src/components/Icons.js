5551ad37447874f3893e4bd71444cb711c823009
hensav <hensav@gmail.com>
2017-06-01 14:39:30 +0300

    added close svg

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -34,3 +34,12 @@ export const Download = (props) => (
     </MuiThemeProvider>
 );
 
+export const Close = (props) => (
+    <MuiThemeProvider>
+        <SvgIcon {...props} style = {defaultStyle}>
+            <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/>
+        </SvgIcon>
+    </MuiThemeProvider>
+);
+
+
