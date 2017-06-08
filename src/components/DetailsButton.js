56d88c660869516c8a8c863db8421806413705e4
vlasn <vlasn@tlu.ee>
2017-06-08 14:50:31 +0300

    Style now in an object

--- src/components/DetailsButton.js
+++ src/components/DetailsButton.js
@@ -12,6 +12,10 @@ export default class DetailsButton extends React.Component {
     }
 
     render() {
+        let style = {
+            //height: '9000px',
+            color: this.props.active === this.props.name ? 'white' : null // Siia k'iks teine v2rv veel
+        };
         return(
         <MuiThemeProvider>
             <FlatButton
@@ -19,7 +23,7 @@ export default class DetailsButton extends React.Component {
                 onTouchTap = {this.clicked.bind(this)}
                 backgroundColor = {this.props.active === this.props.name ? "#00CC33" : null}
                 color = {this.props.active === this.props.name ? "#00CC33" : null}
-                style={this.props.active === this.props.name ? {color:'white',} : null}
+                style={style}
                 hoverColor={"#00CC33"}
             />
         </MuiThemeProvider>
