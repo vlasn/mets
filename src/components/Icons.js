87ce41b49dbdc347f23181ccc2f3e6ccb1ad9df5
Karoliina Roots <karoliinaroots@Karoliinas-MacBook-Air.local>
2017-06-08 14:22:16 +0300

    Added pagination icons

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -43,4 +43,20 @@ export const Close = (props) => (
     </MuiThemeProvider>
 );
 
+export const RightArrow = (props) => (
+    <MuiThemeProvider>
+        <SvgIcon {...props} style = {defaultStyle} viewBox='0 0 18 18'>
+        <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
+        <path d="M0 0h24v24H0z" fill="none"/>
+        </SvgIcon>
+    </MuiThemeProvider>
+);
 
+export const LeftArrow = (props) => (
+    <MuiThemeProvider>
+        <SvgIcon {...props} style = {defaultStyle} viewBox='0 0 18 18'>
+        <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
+        <path d="M0 0h24v24H0z" fill="none"/>
+        </SvgIcon>
+    </MuiThemeProvider>
+);
