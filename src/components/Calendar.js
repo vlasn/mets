ca23296ce19259d7e41e5d2bed13ba7707d3fa1d
Karoliina Roots <karoliinaroots@Karoliinas-MacBook-Air.local>
2017-06-12 09:27:09 +0300

    Added date picker files

---
+++ src/components/Calendar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import DatePicker from './DatePicker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const css = require("./Calendar.scss");
+
+
+const DatePickerExampleSimple = () => (
+  <MuiThemeProvider>
+  <div>
+    <DatePicker/>
+      <div className="Heading">Lisa kuupäevad</div>
+      <div className="Calendar left-align" ><DatePicker floating="Raie" DateTimeFormat={Intl.DateTimeFormat} locale="et-EE" /></div>
+      <div className="Calendar"><DatePicker floating="Väljavedu" DateTimeFormat={Intl.DateTimeFormat} locale="et-EE" /></div>
+      <div className="Calendar right-align"><DatePicker  floating="Raidmete väljavedu" DateTimeFormat={Intl.DateTimeFormat} locale="et-EE" /></div>
+  </div>
+  </MuiThemeProvider>
+
+);
+
+export default DatePickerExampleSimple
---
+++ src/components/DatePicker.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import _DatePicker from 'material-ui/DatePicker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+export default class DatePicker extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+              <_DatePicker
+              floatingLabelText={this.props.floating}
+              DateTimeFormat={Intl.DateTimeFormat}
+              mode="landscape"
+              locale="et-EE"
+              textFieldStyle={{textAlign: 'center' }}
+              style={{ width: '600px' }}
+              cancelLabel="Loobu"
+              className="Muudan"
+              />
+
+        )
+    }
+}
