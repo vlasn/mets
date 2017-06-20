7235bc60ada40e965fb466b8a5d342ba96cfa5cc
hensav <hensav@gmail.com>
2017-06-21 02:02:58 +0300

    input validation logic

--- src/reducers/contractManagment.js
+++ src/reducers/contractManagment.js
@@ -21,6 +21,8 @@ export default function reducer( state = {
             forestMaster: "",
             propertyName: "",
             projectManager: "",
+            email:"",
+            cadastre:""
         }
     },
 
