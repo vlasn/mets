de3a2a6fd459fb00b0e7cc8b807ab76ebc3f5e92
Henry Savi <henry.savi@erply.com>
2017-09-04 17:19:35 +0300

    Contract search bar styling

--- src/components/InputFields/SearchBar.js
+++ src/components/InputFields/SearchBar.js
@@ -4,12 +4,24 @@
 import React from "react"
 import TextField from "material-ui/TextField"
 
+const styles = {
+    underlineStyle: {
+        borderColor: '#9bfcd3',
+    },
+
+    floatingLabelFocusStyle:{
+        color: 'black',
+    }
+};
+
 const SearchBar = (props) => {
     return(
         <TextField
             className="Filter__searchbar"
             hintText="Otsi e-maili/kinnistunime"
             onChange={(e,v)=>props.onChange(v)}
+            underlineFocusStyle={styles.underlineStyle}
+            floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
         />
     )
 }
