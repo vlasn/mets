46cb28b4b350de5099f82b867b96a539c4416ef7
vlasn <vlasn@tlu.ee>
2017-10-10 02:34:20 +0300

    Built inline datepickers and implemented editing contract details. some work on global_success animation.

--- src/components/Contracts/contractActions.js
+++ src/components/Contracts/contractActions.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { session } from "../../utils/Utilities"
+import Api from "../../utils/Api"
 
 export const UPDATE_CONTRACT_ROW_ATTEMPT = "UPDATE_CONTRACT_ROW_ATTEMPT"
 export const UPDATE_CONTRACT_ROW_SUCCESS = "UPDATE_CONTRACT_ROW_SUCCESS"
@@ -8,19 +9,16 @@ export const REFRESH_CONTRACT = "REFRESH_CONTRACT"
 
 
 export function updateContractRow (contractId, key, value) {
-
     return (dispatch) => {
         dispatch({type: UPDATE_CONTRACT_ROW_ATTEMPT,payload: {key: key}})
-        axios.put('/api/contracts/'+contractId, {
-            key: key,
-            value: value,
-        },{
-            ...session()
+        Api('PUT','contract/'+contractId, {
+            [key]: value
         })
-            .then(({data}) => {
-                if(data.status==='accept') {
+            .then(data => {
+                if(data) {
                     dispatch({type: UPDATE_CONTRACT_ROW_SUCCESS, payload: {key: key}})
-                    dispatch({type: REFRESH_CONTRACT, payload: data.data})
+                    dispatch({type: REFRESH_CONTRACT, payload: data})
+                    dispatch({type: "GLOBAL_SUCCESS", text: "Kuupäev muudetud!"})
                 } else {
                     dispatch({type: UPDATE_CONTRACT_ROW_ERROR, payload: {key: key}})
                 }
