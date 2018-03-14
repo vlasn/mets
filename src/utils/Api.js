363154a067c28a2718c3abf4788b319fa4e74f55
hensav <hensav@gmail.com>
2018-03-14 08:41:49 +0200

    implemented stolen progressbar from greg

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,5 +1,14 @@
 import axios from "axios"
 import { session } from "./Utilities"
+import nprogress from 'nprogress'
+
+nprogress.configure({
+  speed: 100,
+  trickleSpeed: 100
+})
+
+let timeout = null
+let isStarted = false
 
 const instance = axios.create({
   baseURL: '/api',
@@ -12,15 +21,42 @@ const allowedMethods = ["GET","POST","PUT","DELETE"]
 export default (method, url, data = {}) => {
 
   if (allowedMethods.indexOf(method.toUpperCase()) > -1) {
+    if (!isStarted) nprogressHandler.start()
 
     return instance({ method, url, data })
-      .then(response => response.data.data)
+      .then(response => {
+        nprogressHandler.delay()
+        return response.data.data
+      })
+
       .catch(error => {
         console.error(error) // and further error logging - db?
+        nprogressHandler.delay()
         return Promise.reject(error)
       })
 
   } else {
     throw new TypeError("Invalid method!")
   }
+}
+
+const nprogressHandler = {
+  start: () => {
+    isStarted = true
+    nprogress.start()
+  },
+  end: () => {
+    isStarted = false
+    timeout = null
+    nprogress.done()
+  },
+  delay: () => {
+    if (timeout && isStarted) {
+      nprogress.inc()
+      clearTimeout(timeout)
+    }
+
+    timeout = nprogressHandler.timer()
+  },
+  timer: () => setTimeout(nprogressHandler.end, 500)
 }
